bfee498cf089403223a62a04b476c09d4b05b2db
diryujin <ujindulanas123@gmail.com>
2026-04-20 16:13:45 +0800

    feat: added index.php and labs1-3

---
+++ lab1.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Lab1</title>
+</head>
+<body>
+  <h1>Laboratory 1</h1>
+  <h2>This is my lab 1</h2>
+</body>
+</html>
